bd18edb4a35fab4265601bea77f72533b42bceba
xhezh <fedyanin14@gmail.com>
2024-11-10 02:13:46 +0300

    feat: project setup

---
+++ Assets/_Source/Core/Sample.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Core
+{
+    public class Sample : MonoBehaviour
+    {
+        
+    }
+}
---
+++ Assets/_Source/Core/asd.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+namespace Core
+{
+    public class Asd : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        private void Start()
+        {
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+        }
+    }
+}
